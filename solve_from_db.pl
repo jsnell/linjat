30f06323c061b045cdf303a1d0440b41b70a861e
Juho Snellman <jsnell@iki.fi>
2019-03-18 01:04:07 +0100

    Move solve_from_db from shell to perl

---
+++ solve_from_db.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use File::Slurp;
+use JSON;
+
+my $diff = shift;
+my $index = shift;
+
+my $puzzles = decode_json scalar read_file 'www/data/puzzles.json';
+my $puzzle = $puzzles->{$diff}[$index -1];
+my $str = join ',', '', @{$puzzle->{puzzle}};
+
+my $w = length $puzzle->{puzzle}[0];
+my $h = scalar @{$puzzle->{puzzle}};
+my $p = ("$str") =~ tr/0-9//;
+
+die if system "MAP_HEIGHT=$h MAP_WIDTH=$w PIECES=$p cmake . && make ";
+system qq(bin/mklinjat --solve="$str");
+
+    
+    
